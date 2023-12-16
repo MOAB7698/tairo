8582be7ad0a09efee9935754ad36120b4b705d12
MOAB7698 <41636459+MOAB7698@users.noreply.github.com>
2023-12-16 09:46:40 +0330

    1

--- .demo/utils/apps/calendar/composables/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './now'
-export * from './pan'
-export * from './create-event'
-export * from './drag-event'
-export * from './events'
-export * from './drag-event-pending'
-export * from './filters'
--- .demo/utils/apps/calendar/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './types'
-export * from './view'
-export * from './composables'
-export * from './category-theme'
